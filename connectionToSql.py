036da3c0da7e5e33d03788ac63144a1c3123e60d
Henry Posada <henryposada@mail.adelphi.edu>
2020-11-22 18:01:59 -0500

    PythonSQLConnection Commit

---
+++ connectionToSql.py
@@ -0,0 +1,14 @@
+import pandas
+
+import mysql.connector
+
+conn = mysql.connector.connect(user='root',password='root_password',host='69.121.70.211',database='ticketingSystem',auth_plugin='mysql_native_password')
+
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM ticketingSystem.test')
+
+for row in cursor:
+    print(row)
+    
