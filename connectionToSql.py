bac932c3f05868536a5b3ec1c248da04a3867b43
Henry Posada <henryposada@mail.adelphi.edu>
2020-12-02 19:07:03 -0500

    Updates to python script

--- connectionToSql.py
+++ connectionToSql.py
@@ -1,7 +1,5 @@
 import pandas
-
 import mysql.connector
-
 from datetime import datetime
 now = datetime.now()
 formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
@@ -10,43 +8,61 @@ conn = mysql.connector.connect(user='root',password='root_password',host='69.121
 
 cursor = conn.cursor()
 
-def queryByID(idNumber):
-    ticket = []
-    cursor.execute('SELECT * FROM ticketingSystem.tickets WHERE ID = '+str(idNumber))
+def sqlToArray(cursor):
+    array = []
     for row in cursor:
-        ticket.append(row)
+        array.append(row)   
+    return array
+
+# queryByID: This function uses passed ID number and checks if it exists in the DB.
+# Return String: A string is returned when the ID number does not match a ticket in the DB.
+# Return Array: An array is returned if the ID number exists in the DB.
+def queryByID(idNumber):
+    cursor.execute('SELECT * FROM ticketingSystem.tickets WHERE ID = '+str(idNumber))
+    ticket = sqlToArray(cursor)
     return ticket
 
+# queryByKeyword: This function uses passed word and checks if it char/string exists in ANY ticket name or description.
+# Return Array: An array is returned if the word is associated with a ticket, the array is the ticket(s) that contains the word.
+# Return String: A string is returned if the word is not associated with a ticket, the string informs that no such ticket does not exist.
 def queryByKeyword(words):
-    tickets = []
     cursor.execute("SELECT * FROM ticketingSystem.tickets WHERE name LIKE '%s' OR description LIKE '%s'" % (str('%'+words+'%'), str('%'+words+'%')))
-    for row in cursor:
-        tickets.append(row)
-    return tickets
-
-def queryByStatus(status):
-    tickets = []
-    cursor.execute("SELECT * FROM ticketingSystem.tickets WHERE status = '%s'" % str(status))
-    for row in cursor:
-        tickets.append(row)
-    return tickets
-
-def updateStatusOfTicket(ticketID, ticketStatus):
-    if(ticketStatus.lower() == 'open'):
-        ticketStatus = 'Closed'
+    tickets = sqlToArray(cursor)
+    if not tickets:
+        return 'The entered word does not exist in any existing ticket.'
     else:
-        ticketStatus = 'Open'
-    cursor.execute("UPDATE ticketingSystem.tickets SET status = '%s' WHERE ID = %s" % (str(ticketStatus), str(ticketID)))
+        return tickets
+
+# queryByStatus: This function uses passed status and returns all tickets with that status.
+# Return Array: An array is returned if the status corresponds with a ticket in the DB, this array is the ticket(s) with the matching status.
+# Return String: A String is returned if there is no ticket in the DB with passed status.
+def queryByStatus(status):
+    cursor.execute("SELECT * FROM ticketingSystem.tickets WHERE status = '%s'" % str(status))
+    tickets = sqlToArray(cursor)
+    return tickets
+
+# updateStatusOfTicket: This function takes a uses the ticket ID to locate the ticket and then updates the ticket status based on the existing status.
+# Return string: Returns appropriate string based on if the ticket ID given exists in the Database.
+def updateStatusOfTicket(ticketID, ticketStatus):
+    checkExists = queryByID(ticketID)
+    if not checkExists:
+        return 'There is no ticket associated with this ticket ID.'
+    else:
+        if(ticketStatus.lower() == 'open'):
+            ticketStatus = 'Closed'
+        else:
+            ticketStatus = 'Open'
+        cursor.execute("UPDATE ticketingSystem.tickets SET status = '%s' WHERE ID = %s" % (str(ticketStatus), str(ticketID)))
+        return 'Ticket Updated'
+    
 
 # doesUserExist: This function takes a username and checks against our database to see if there's a match.
-# Return true: The inputted username exists in our SQL database
-# Return false: The inputted username does not exist in our SQL database
+# Return true: The inputted username exists in our SQL database.
+# Return false: The inputted username does not exist in our SQL database.
 def doesUserExist(userName): 
     cursor.execute("SELECT * FROM ticketingSystem.users WHERE userName = '%s'" % str(userName))
-    user = []
-    for row in cursor:
-        user.append(row)
-    if len(user) == 0:
+    user = sqlToArray(cursor)
+    if not user:
         return False
     else:
         return True
@@ -57,7 +73,7 @@ def doesUserExist(userName):
 def addTicket(name, description, status):
     sql = "INSERT INTO tickets (name, description, date, status) VALUES (%s, %s, %s, %s)"
     val = (str(name), str(description), formatted_date, str(status))
-    cursor.execute(sql, val)    
+    cursor.execute(sql, val)
 
 #searcher = "closed"
 
