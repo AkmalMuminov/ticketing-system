3837e3c00185c40e4f81b2437b65f9c84d4a9900
Henry Posada <henryposada@mail.adelphi.edu>
2020-12-08 13:02:35 -0500

    Final Push Seriously

--- newTicket.php
+++ newTicket.php
@@ -5,7 +5,7 @@
     if(empty($name) or empty($descrip) or empty($category))
         die('Ticket could not be created. Please complete all fields before submitting.');
     else{
-        $command = escapeshellcmd("py -c \"import connectionToSql; connectionToSql.addTicket('".$name."','".$descrip."','".'open'."','".'N/A'."','".$category."')\"");
+        $command = escapeshellcmd("py -c \"import connectionToSql; connectionToSql.addTicket('".$name."','".$descrip."','".'Open'."','".'N/A'."','".$category."')\"");
         
         $output = shell_exec($command);
         echo 'Ticket has been created successfully';
