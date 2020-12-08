6e0a3a6e97ebca25a81d4164a610bc30520ef3d4
AkmalMuminov <akmalmuminov@mail.adelphi.edu>
2020-12-07 20:59:48 -0500

    Some styling changes

--- format.py
+++ format.py
@@ -1,16 +1,19 @@
 import datetime
 x = [(1, 'test2', '2', datetime.date(2020, 12, 2), 'closed'), (2, 'test1', '1', datetime.date(2020, 11, 2), 'closed')]
 
-newList = []
-n=3#index of datetime
-for i in x:
+
+def format (x):
+
+    newList = []
+    n=3#index of datetime
+    for i in x:
         a = list(i) #convert to list
         b = a.pop(n) #remove datetime element
         a.insert(n,b.strftime('%m/%d/%Y')) #put str date
         newList.append(a)
-print(newList)
+    #print(newList)
 
-newList=tuple(newList)
-print (newList)
+    newList=tuple(newList)
+    print (newList)
 
-    
+format(x)
